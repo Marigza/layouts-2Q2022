b6c5b3b63582dfcdb15d6ff43359dbfddb1629ff
Marina Gapeyeva <marigola@mail.ru>
2022-07-03 11:56:46 +0300

    feat: add close-feature to burger-item account

--- travel/index.js
+++ travel/index.js
@@ -7,10 +7,14 @@ console.log("Total points 108: \n done: 23 items; \n not done: 1 item (favicon)"
     const burgerItem = document.querySelector('.burger');
     const menu = document.querySelector('.header_nav');
     const menuCloseItem = document.querySelector('.header_nav-close');
+    const accountClose = document.querySelector('.account');
     burgerItem.addEventListener('click', () => {
         menu.classList.add('header_nav_active');
     });
     menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
+    accountClose.addEventListener('click', () => {
         menu.classList.remove('header_nav_active');
     });
 }());
