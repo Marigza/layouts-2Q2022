d3772944652174bdadffd3222d6272f7a20241ef
Marina Gapeyeva <marigola@mail.ru>
2022-07-03 11:18:17 +0300

    feat: add burger-menu

--- travel/index.js
+++ travel/index.js
@@ -1,3 +1,16 @@
 
 console.log("Total points 108: \n done: 23 items; \n not done: 1 item (favicon)");
 
+// burger handler
+
+(function () {
+    const burgerItem = document.querySelector('.burger');
+    const menu = document.querySelector('.header_nav');
+    const menuCloseItem = document.querySelector('.header_nav-close');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('header_nav_active');
+    });
+    menuCloseItem.addEventListener('click', () => {
+        menu.classList.remove('header_nav_active');
+    });
+}());
