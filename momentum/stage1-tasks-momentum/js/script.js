bd236925e4faf56988aa38b3a958f5c6b057875a
Marina Gapeyeva <marigola@mail.ru>
2022-08-09 12:05:09 +0300

    feat: create greeting

--- momentum/stage1-tasks-momentum/js/script.js
+++ momentum/stage1-tasks-momentum/js/script.js
@@ -1,5 +1,9 @@
 const time = document.querySelector('.time');
 const day = document.querySelector('.date');
+const greet = document.querySelector('.greeting');
+const userName = document.querySelector('.name');
+
+
 
 function showDate() {
     const date = new Date();
@@ -8,11 +12,43 @@ function showDate() {
     day.textContent = currentDate;
 }
 
+function getTimeOfDay() {
+    const date = new Date();
+    const hours = date.getHours();
+    let timeOfDay;
+
+    if (hours >= 0 && hours < 6) {
+        return timeOfDay = 'night'
+    } else if (hours >= 6 && hours < 12) {
+        return timeOfDay = 'morning'
+    } else if (hours >= 12 && hours < 18) {
+        return timeOfDay = 'afternoon'
+    } else if (hours >= 12 && hours < 18) {
+        return timeOfDay = 'evening'
+    }
+}
+function showGreeting() {
+    greet.textContent = `Good ${getTimeOfDay()},`
+}
+
 function showTime() {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
     showDate();
+    showGreeting();
 }
 showTime();
+
+function setLocalStorage() {
+    localStorage.setItem('userName', userName.value);
+}
+window.addEventListener('beforeunload', setLocalStorage);
+
+function getLocalStorage() {
+    if (localStorage.getItem('userName')) {
+        userName.value = localStorage.getItem('userName');
+    }
+}
+window.addEventListener('load', getLocalStorage);
