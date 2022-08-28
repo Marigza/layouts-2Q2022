dcf3e1e3173b45e99aa99ceee4538b520c33c2d7
Marina Gapeyeva <marigola@mail.ru>
2022-08-28 18:29:51 +0300

    refactor: delete unused file and strings of code

--- codejam-eldritch/js/index.js
+++ codejam-eldritch/js/index.js
@@ -1,23 +1,66 @@
+console.log('Pеализация:\n-Карты замешиваются согласно правилам игры\n-Есть трекер текущего состояния колоды\n-Одна карта древнего\n-Один уровень сложности\n-Также в консоль выведен массив карт, которые играют в партии, старт с последнего элемента')
 
+// выбор древнего.
 
-import { blueCardsData, greenCardsData, brownCardsData } from './data.js';
-//console.log(blueCardsData[1].src);
-
-// выбор древнего. реализация не доделана...
-/*
 const ancient = Array.from(document.querySelectorAll('.ancient'))
-const activAncient = document.querySelector('.ancient-active')
-function selectAncient() {
-    for (let item of ancient) {
-        item.addEventListener('click', () => {
-                item.classList.add('ancient-active')
-                console.log(item)
-            })
-        }
-    }
-      
-selectAncient()
-*/
+
+const azathot = [{
+    greenCards: 1,
+    blueCards: 1,
+    brownCards: 2,
+},
+{
+    greenCards: 2,
+    blueCards: 1,
+    brownCards: 3,
+},
+{
+    greenCards: 2,
+    blueCards: 0,
+    brownCards: 4,
+    }];
+
+const cthulthu = [{
+    greenCards: 0,
+    blueCards: 2,
+    brownCards: 2,
+},
+{
+    greenCards: 1,
+    blueCards: 0,
+    brownCards: 3,
+},
+{
+    greenCards: 3,
+    blueCards: 0,
+    brownCards: 4,
+    }];
+
+const cardInGameGreen1 = document.querySelector('.green1')
+const cardInGameBrown1 = document.querySelector('.brown1')
+const cardInGameBlue1 = document.querySelector('.blue1')
+
+const cardInGameGreen2 = document.querySelector('.green2')
+const cardInGameBrown2 = document.querySelector('.brown2')
+const cardInGameBlue2 = document.querySelector('.blue2')
+
+const cardInGameGreen3 = document.querySelector('.green3')
+const cardInGameBrown3 = document.querySelector('.brown3')
+const cardInGameBlue3 = document.querySelector('.blue3')
+
+let currentAncient = azathot
+//let currentAncient = cthulthu
+
+cardInGameGreen1.textContent = currentAncient[0].greenCards
+cardInGameBrown1.textContent = currentAncient[0].brownCards
+cardInGameBlue1.textContent = currentAncient[0].blueCards
+cardInGameGreen2.textContent = currentAncient[1].greenCards
+cardInGameBrown2.textContent = currentAncient[1].brownCards
+cardInGameBlue2.textContent = currentAncient[1].blueCards
+cardInGameGreen3.textContent = currentAncient[2].greenCards
+cardInGameBrown3.textContent = currentAncient[2].brownCards
+cardInGameBlue3.textContent = currentAncient[2].blueCards
+
 // установка трека в зависимости от имени древнего
 
 // выбор уровня сложности
@@ -33,9 +76,9 @@ const openCard = document.querySelector('.open')
 function mixTheDeck() {
     mixingDeck.classList.add('button-active')
     closeCard.classList.remove('close')
-    openCard.classList.remove('close')
+    openCard.classList.remove('open')
     closeCard.classList.add('close-active')
-    openCard.classList.add('close-active')
+    openCard.classList.add('open-active')
 }
 
 mixingDeck.addEventListener('click', mixTheDeck)
@@ -46,22 +89,9 @@ const green = ['green1', 'green2', 'green3', 'green4', 'green5', 'green6', 'gree
 const blue = ['blue1', 'blue2', 'blue3', 'blue4', 'blue5', 'blue6', 'blue7', 'blue7', 'blue8', 'blue10', 'blue11', 'blue12',];
 const brown = ['brown1', 'brown2', 'brown3', 'brown4', 'brown5', 'brown6', 'brown7', 'brown8', 'brown9', 'brown10', 'brown11', 'brown12', 'brown13', 'brown14', 'brown15', 'brown16', 'brown17', 'brown18', 'brown19', 'brown20', 'brown21',]
 
-const azatot = [ {  greenCards: 1,
-                    blueCards: 1,
-                    brownCards: 2,
-                },
-                 {  greenCards: 2,
-                    blueCards: 1,
-                    brownCards: 3,
-                },
-                {   greenCards: 2,
-                    blueCards: 0,
-                    brownCards: 4,
-                }]
-
-const greenSum = azatot[0].greenCards + azatot[1].greenCards + azatot[2].greenCards
-const blueSum = azatot[0].blueCards + azatot[1].blueCards + azatot[2].blueCards
-const brownSum = azatot[0].brownCards + azatot[1].brownCards + azatot[2].brownCards
+const greenSum = currentAncient[0].greenCards + currentAncient[1].greenCards + currentAncient[2].greenCards
+const blueSum = currentAncient[0].blueCards + currentAncient[1].blueCards + currentAncient[2].blueCards
+const brownSum = currentAncient[0].brownCards + currentAncient[1].brownCards + currentAncient[2].brownCards
 
 // случайный выбор карт одного цвета в нужном для партии количестве
 
@@ -75,25 +105,20 @@ function shuffle(array) {
 shuffle(green);
 let newGreen = green.slice(0, greenSum)
 shuffle(newGreen);
-//console.log(newGreen)
 
 shuffle(blue);
 let newBlue = blue.slice(0, blueSum)
 shuffle(newBlue);
-//console.log(newBlue)
 
 shuffle(brown);
 let newBrown = brown.slice(0, brownSum)
 shuffle(newBrown);
-//console.log(newBrown)
 
-// !!!отбор карт по цветам и количеству в первую стадию. перемешивание этих карт.
+// отбор карт по цветам и количеству в первую стадию. перемешивание этих карт.
 
-const firstStage = newGreen.slice(0, azatot[0].greenCards) + ',' + newBlue.slice(0, azatot[0].blueCards) + ',' + newBrown.slice(0, azatot[0].brownCards)
-const secondStage = newGreen.slice(azatot[0].greenCards, azatot[1].greenCards + azatot[0].greenCards) + ',' + newBlue.slice(azatot[0].blueCards, azatot[1].blueCards + azatot[0].blueCards) + ',' + newBrown.slice(azatot[0].brownCards, azatot[1].brownCards + azatot[0].brownCards)
-const thirdStage = newGreen.slice(azatot[1].greenCards + azatot[0].greenCards, azatot[2].greenCards + azatot[1].greenCards + azatot[0].greenCards) + ',' + newBlue.slice(azatot[1].blueCards + azatot[0].blueCards, azatot[2].blueCards + azatot[1].blueCards + azatot[0].blueCards) + ',' + newBrown.slice(azatot[1].brownCards + azatot[0].brownCards, azatot[2].brownCards + azatot[1].brownCards + azatot[0].brownCards)
-
-//console.log(firstStage)
+const firstStage = newGreen.slice(0, currentAncient[0].greenCards) + ',' + newBlue.slice(0, currentAncient[0].blueCards) + ',' + newBrown.slice(0, currentAncient[0].brownCards)
+const secondStage = newGreen.slice(currentAncient[0].greenCards, currentAncient[1].greenCards + currentAncient[0].greenCards) + ',' + newBlue.slice(currentAncient[0].blueCards, currentAncient[1].blueCards + currentAncient[0].blueCards) + ',' + newBrown.slice(currentAncient[0].brownCards, currentAncient[1].brownCards + currentAncient[0].brownCards)
+const thirdStage = newGreen.slice(currentAncient[1].greenCards + currentAncient[0].greenCards, currentAncient[2].greenCards + currentAncient[1].greenCards + currentAncient[0].greenCards) + ',' + newBlue.slice(currentAncient[1].blueCards + currentAncient[0].blueCards, currentAncient[2].blueCards + currentAncient[1].blueCards + currentAncient[0].blueCards) + ',' + newBrown.slice(currentAncient[1].brownCards + currentAncient[0].brownCards, currentAncient[2].brownCards + currentAncient[1].brownCards + currentAncient[0].brownCards)
 
 let first = firstStage.split(',');
 let second = secondStage.split(',');
@@ -118,10 +143,6 @@ shuffle(third);
 
 let readyStack = third.concat(second, first)
 
-//console.log(first)
-//console.log(second)
-//console.log(third)
-
 console.log(readyStack) //стек готовый к игре
 
 // открытие поочередно карт из получившейся колоды. трекер окончания стадии, связь трекера оставшихся карт с уже открытыми картами.
@@ -131,18 +152,6 @@ const stage1 = document.querySelector('.first')
 const stage2 = document.querySelector('.second')
 const stage3 = document.querySelector('.third')
 let visibleCard;
-
-const cardInGameGreen1 = document.querySelector('.green1')
-const cardInGameBrown1 = document.querySelector('.brown1')
-const cardInGameBlue1 = document.querySelector('.blue1')
-
-const cardInGameGreen2 = document.querySelector('.green2')
-const cardInGameBrown2 = document.querySelector('.brown2')
-const cardInGameBlue2 = document.querySelector('.blue2')
-
-const cardInGameGreen3 = document.querySelector('.green3')
-const cardInGameBrown3 = document.querySelector('.brown3')
-const cardInGameBlue3 = document.querySelector('.blue3')
 
 function scanColorOfCard() {
     while (readyStack.length >= second.length + third.length) {
@@ -169,7 +178,7 @@ function scanColorOfCard() {
 }
 
 function showCurrentCard() {
-    if (readyStack.length !== 0) {
+    if (readyStack.length > 0) {
         visibleCard = readyStack.pop()
         image.src = `./assets/MythicCards/${visibleCard}.jpg`;
             if (readyStack.length === second.length + third.length) {
@@ -177,7 +186,7 @@ function showCurrentCard() {
             } else if (readyStack.length === third.length) {
                 stage2.classList.add('stage_is_over')
             }
-        console.log(visibleCard)
+        //console.log(visibleCard)
         scanColorOfCard()
         //console.log(readyStack.length)
         return visibleCard
@@ -187,6 +196,4 @@ function showCurrentCard() {
     }
 }
 
-closeCard.addEventListener('click', showCurrentCard)
-
-
+closeCard.addEventListener('click', showCurrentCard) 
